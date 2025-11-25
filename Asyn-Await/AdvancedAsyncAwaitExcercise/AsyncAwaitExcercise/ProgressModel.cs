6db68aefaf33ff1a5f4b001281610cc12242d191
Mohammed Abubakkar Siddiq <mohammedabubakkar0701@gmail.com>
2025-11-25 15:39:05 +0530

    Final commit

---
+++ Asyn-Await/AdvancedAsyncAwaitExcercise/AsyncAwaitExcercise/ProgressModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitExcercise
+{
+    public class ProgressModel
+    {
+        public int PercentageRead { get; set; } = 0;
+        public List<FileDataModel> ReadCompletedFiles { get; set; } = new List<FileDataModel>();
+    }
+}
